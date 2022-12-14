2f281001f3be58043cc82011561338c7b47d8dd8
rose534 <wambuirose534@gmail.com>
2022-12-14 20:08:35 +0300

    git

--- js/index.js
+++ js/index.js
@@ -1,54 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form')
-    form.addEventListener('submit', e => {
-        e.preventDefault()
-        const users = form.search.value
-        getGitUser(users)
-        form.reset()
+document.addEventListener("DOMContentLoaded", () => {
+    
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      searchInput(e.target.search.value)
     })
 })
-function getGitUser(username){
-    fetch('https://api.github.com/search/users?q=${username}', {method: 'GET', headers: {
-        Accept: 'application/vnd.github.v3+json'
-    }}).then(res => res.json()).then(data => {
-        const users = data.items
-        users.forEach(user => {
-            userList(user)
-        });
+const config ={
+    header:{
+        "Content-Type": "application/json",
+        Accept: "application/vnd.github.v3+json"
+    }
+}
+function searchInput(input){
+    fetch(`https://api.github.com/search/users?q=${input}`)
+    .then(response => response.json())
+    .then(data =>{
+        let name = data.items
+        console.log(name)
+        const lp = ()=>{
+            for(let i = 0; i <name.length;i++){
+                const user = document.querySelector('#user-list')
+                const li1 = document.createElement('li')
+                li1.innerHTML=`<li>${name[i].login}</li><p>${name[i].html_url}<p><img src=${name[i].avatar_url}>`
+                user.appendChild(li1)
+                li1.addEventListener("click",()=>{
+                    fetch(`https://api.github.com/users/${name[i].login}/repos`)
+                    .then(response=>response.json())
+                    .then(repos=>{
+                        console.log(repos)
+                        for(let i = 0; i <repos.length;i++){
+                            const reposi =document.querySelector("#repos-list")
+                            const repoli =document.createElement('li')
+                            repoli.innerHTML = `${repos[i].name}`
+                            reposi.appendChild(repoli)
+                        }
+                    })
+                })
+            }
+        }
+        lp()
     })
 }
-function getUserRepos(url){
-    fetch(url, {method: 'GET', headers: {
-        Accept: 'application/vnd.github.v3+json'
-    }}).then(res => res.json()).then(data => userRepos(data))
-}
-function userList(user){
-    const userUl = document.getElementById('user-list')
-    const userLi = document.createElement('li')
-    userLi.style.padding = '15px'
-    userLi.innerHTML =
-    `
-    <img src='${user.avatar_url}' width='100' height='100' alt='avatar' />
-    <h2 id='${user.login}'>${user.login}</h2>
-    `
-    userUl.appendChild(userLi)
-    document.getElementById('${user.login}').addEventListener('click', () => {
-        const repoUrl = '${user.repos_url}'
-        getUserRepos(repoUrl)
-    })
-}
-function userRepos(repos){
-    const reposUl = document.getElementById('repos-list')
-    repos.map(repo => {
-        const reposLi = document.createElement('li')
-        reposLi.textContent = repo.name
-        console.log(repo.name);
-        reposUl.appendChild(reposLi)
-    })
-}
-
-
-
-
-
-
